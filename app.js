797b651b6127fc8d40944ca00cef99790c6447c0
acr012 <acr012@ucsd.edu>
2016-11-02 13:40:04 -0700

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 app.use(express.logger('dev'));
-app.use(bodyParser.json());
+app.use(express.bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
