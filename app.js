6ca2a36bfa943faa816d07d5bd49e0ae99c60430
acr012 <acr012@ucsd.edu>
2016-11-02 17:37:57 -0700

    app mod

--- app.js
+++ app.js
@@ -1,30 +1,57 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+ar express = require('express');
 var http = require('http');
+var path = require('path');
+// var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
+// Example route
+// var user = require('./routes/user');
 
 var app = express();
 
-// view engine setup
+// all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use(express.favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser());
 app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+
+// var express = require('express');
+// var path = require('path');
+// var favicon = require('serve-favicon');
+// var logger = require('morgan');
+// var cookieParser = require('cookie-parser');
+// var bodyParser = require('body-parser');
+// var http = require('http');
+
+// var index = require('./routes/index');
+
+// var app = express();
+
+// // view engine setup
+// app.set('port', process.env.PORT || 3000);
+// app.set('views', path.join(__dirname, '/views'));
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'html');
+// app.use(express.favicon());
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.session());
+// app.use(app.router);
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', index);
 
 // // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
